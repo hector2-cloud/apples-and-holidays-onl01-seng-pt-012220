9c1061aec7d345cf39c8221e0678a52ff69543b6
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-02-03 01:39:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ holiday_hash.each do |season, h|
     new_str = str.map {|w| w.capitalize }.join(" ")
   
   supply_str = array.join(", ")
-  puts " #{new_str}: #{supply_str}"
+  puts "  #{new_str}: #{supply_str}"
 end
 end
 end
